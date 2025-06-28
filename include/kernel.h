3f5d3d7fb19043082c585434a6c2c12abf320990
revelwivanto <revel@LAPTOP-7UK6NOVC.>
2025-06-29 00:13:47 +0700

    success

--- include/kernel.h
+++ include/kernel.h
@@ -3,11 +3,8 @@
 
 #include "std_type.h"
 
-// extern int interrupt(int number, int AX, int BX, int CX, int DX);
-extern int interrupt10(int AX, int BX, int CX, int DX);
-extern int interrupt13(int AX, int BX, int CX, int DX);
-extern int interrupt16(int AX, int BX, int CX, int DX);
-extern int interrupt21(int AX, int BX, int CX, int DX);
+extern void putInMemory(int segment, int address, char character);
+extern int interrupt(int number, int AX, int BX, int CX, int DX);
 
 void printString(char* str);
 void readString(char* buf);
@@ -16,4 +13,4 @@ void clearScreen();
 void readSector(byte* buf, int sector);
 void writeSector(byte* buf, int sector);
 
-#endif
+#endif // __KERNEL_H__
